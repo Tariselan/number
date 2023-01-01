8b2c5772c59d39fdf5abce5f4375f1b3b6bc64d7
krv1974 <63528758+krv1974@users.noreply.github.com>
2023-01-01 14:07:53 +1300

    Fixed the Code

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 window.onload = function() {
-    document.getElementById('number').value = 123;
+    document.getElementById('number').value = 0;
     document.getElementById('base').value = 10;
     document.getElementById('output').value;
 }
@@ -15,13 +15,20 @@ function convertToParts() {
     let number = document.getElementById('number').value;
     let base = document.getElementById('base').value;
     document.getElementById('output').value = '';
-    
+    if (number == 0) {
+        document.getElementById('output').value = 'Zero';
+        return; 
+    }
+    if (base <= 1) {
+        document.getElementById('output').value = 'Base is too hard';
+        return;
+    }
     size = Math.ceil((ln(number))/(ln(base))) - 1; // 69's largest index is 10^1
     for (let i = 0; i < size+1; i++) {
         //base**(size-i)
         let x = Math.floor(number/(base**(size-i)))*(base**(size-i));
-        x = x / (base);
-        document.getElementById('output').value += x + ' ';
+        let y = x / (base**(size-i));
+        document.getElementById('output').value +=  y + ' ';
         number -= x;
     }
 }
